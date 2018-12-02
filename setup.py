fc0b0212f84367f1e42f0f0854f3b491c923dadd
devin <zhangchuzhao@youmi.net>
2018-12-02 10:36:51 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #!/usr/env/bin python
-#-*- coding: utf-8 -*-
-
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 setup(
     name="xmind",
-    version="0.1a.0",
+    version="0.1.0",
     packages=find_packages(),
 
     install_requires=["distribute"],
 
-    author="Woody Ai",
-    author_email="aiqi@xmind.net",
-    description="The offical XMind python SDK",
+    author="devin",
+    author_email="1324556701@qq.com",
+    description="Xmind思维导图创建和解析的一站式解决方案",
     license="MIT",
-    keywords="XMind, SDK, mind mapping",
+    keywords="xmind, mind mapping, 思维导图",
     url="https://github.com/xmindltd/xmind-sdk-python"
 )
