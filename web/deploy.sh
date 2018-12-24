248706044db5a5c1410b7e99ecfdcdea0bc13842
devin <zhangchuzhao@youmi.net>
2018-12-24 20:22:52 +0800

    refector: split the testcase convertion tool

--- web/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo start deploy...
-
-kill -HUP `cat application.pid`
-kill `cat application.pid`
-gunicorn application:app -p application.pid -b 0.0.0.0:8000 -w 4 -D
-
-echo finish deploy...
--- web/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists records;
-
-create table records (
-  id integer primary key autoincrement,
-  name text not null,
-  create_on text not null,
-  note text,
-  is_deleted integer DEFAULT 0
-);
