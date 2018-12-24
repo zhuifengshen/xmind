0d8ab300b13314e1fb915813257874690f74231b
devin <zhangchuzhao@youmi.net>
2018-12-24 20:03:09 +0800

    chore: update deploy.sh

--- web/deploy.sh
+++ web/deploy.sh
@@ -4,11 +4,6 @@ echo start deploy...
 
 kill -HUP `cat application.pid`
 kill `cat application.pid`
-
-cd ~/project/xmind
-export PYTHONPATH=`pwd`
-
-cd ~/project/xmind/web
 gunicorn application:app -p application.pid -b 0.0.0.0:8000 -w 4 -D
 
 echo finish deploy...
