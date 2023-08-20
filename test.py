220cc05c732b3d23d59b2e2a36788e867e29ff18
MasterYip <2205929492@qq.com>
2023-08-20 14:57:24 +0800

    refactor: arrange file sturcture

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-
-import xmind
-from fmt_cvt.md2xmind import MarkDown2Xmind
+from XmindCopilot import xmind
+from XmindCopilot.fmt_cvt.md2xmind import MarkDown2Xmind
 
 # md格式的源文件路径
-file_path = os.path.abspath(
-    "E:\\CodeTestFile\\Github-opensource-repo\\XmindCopilot\\test\\SLIP.md")
-xmind_path = "E:\\CodeTestFile\\Github-opensource-repo\\XmindCopilot\\test\\md2xmind_SLIP.xmind"
+file_path = "E:\\CodeTestFile\\Github-opensource-repo\\XmindCopilotPrj\\XmindCopilot\\test\\SLIP.md"
+xmind_path = "E:\\CodeTestFile\\Github-opensource-repo\\XmindCopilotPrj\\XmindCopilot\\test\\md2xmind_SLIP.xmind"
 workbook = xmind.load(xmind_path)
 rootTopic = workbook.getPrimarySheet().getRootTopic()
 markdowntext = open(file_path, 'r', encoding='utf-8').read()
