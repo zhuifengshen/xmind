def5dedefdf69e80a309a35e49af711975772404
MasterYip <2205929492@qq.com>
2023-08-21 16:45:16 +0800

    fix: remove lowercase xmindcopilot trackings

--- xmindcopilot/__about__.py
+++
@@ -1,9 +0,0 @@
-__title__ = 'XMindCopilot'
-__description__ = 'XMind是基于Python实现，提供了对XMind思维导图进行创建、解析、更新的一站式解决方案！'
-__keywords__ = 'xmind, mind mapping, 思维导图, XMind思维导图',
-__url__ = 'https://github.com/zhuifengshen/xmind'
-__author__ = 'MasterYip'
-__author_email__ = '2205929492@qq.com'
-__version__ = '0.0.1'
-__license__ = 'MIT'
-__cake__ = u'\u2728 \U0001f370 \u2728'
--- xmindcopilot/topic_cluster/TextCluster/__init__.py
+++
@@ -1,3 +0,0 @@
-# from .utils.similar import jaccard
-# from .utils.segmentor import Segmentor
-# from .utils.utils import check_file, ensure_dir, clean_dir, sample_file, get_stop_words, line_counter, Range
